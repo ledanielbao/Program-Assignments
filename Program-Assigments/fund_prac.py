2ade07a9c2fa3829439d5d18d2e96261655606b9
Daniel Le <ledanielbao@gmail.com>
2021-06-16 09:39:49 -0700

    declared new variables

--- Program-Assigments/fund_prac.py
+++ Program-Assigments/fund_prac.py
@@ -1,1 +1,19 @@
 print("hello world")
+# print hello world
+
+name = "Lebron" # declaring name variable
+age = 35 #declared age var
+salary = 37.44 # declared salary var
+plays_basketball = True # declared boolean var
+jersey_number = "23" # declared jersey number
+
+# STEP 2
+car_name = "Toyota"
+x = 50
+my_first_name = "John"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
